f19d1e173eeaa5b979a59dce6ab92838f6916fb4
karinlee <karinlee92@gmail.com>
2023-01-03 13:20:46 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// Addition
+let addition_operator = 1 + 1; //expected result: 2
+console.log("addition: " + addition_operator);
+
+// Subtraktion
+let subtraktion_operator = 2 - 1; //expected result: 1
+console.log("subtraktion: " + subtraktion_operator);
+
+// Multiplikation
+let multiplication_operator = 2 * 2; //expected result: 4
+console.log("multiplication: " + multiplication_operator);
+
+// Division
+let division_operator = 4 / 2; //expected relsult: 2
+console.log("division: " + division_operator);
+
+// Modul: zeigt an, was der Rest sein würde.
+let modulus_operator = 14 % 5; //expected result: 4
+console.log("modulus: " + modulus_operator);
